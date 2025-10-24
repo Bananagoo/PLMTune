01cb1ab84e74fcb39a559193fd69223516820f0a
Katherine Langille <katherine.langille@sickkids.ca>
2025-10-24 13:02:16 -0400

    Fix ESM loader to use local checkpoint

--- src/idr_vep/utils/esm_utils.py
+++ src/idr_vep/utils/esm_utils.py
@@ -2,14 +2,12 @@ import torch
 from esm import pretrained
 
 def load_esm(model_name: str = "esm2_t33_650M_UR50D", freeze: bool = True):
-    """
-    Load a local ESM-2 model checkpoint.
-    Returns: (model, alphabet, batch_converter, embed_dim, repr_layer)
-    """
     local_path = "/hpf/largeprojects/tcagstor/tcagstor_tmp/klangille/PLMTune/models/esm2_t33_650M_UR50D.pt"
+    print(f"Loading ESM-2 model directly from local path: {local_path}")
 
-    print(f"Loading ESM-2 model from local path: {local_path}")
-    model, alphabet = pretrained.load_model_and_alphabet_local(local_path)
+    model_data = torch.load(local_path, map_location="cpu")
+    model, alphabet = pretrained.load_model_and_alphabet_core(model_data, regression_data=None)
+
     repr_layer = 33
     embed_dim = 1280
 
